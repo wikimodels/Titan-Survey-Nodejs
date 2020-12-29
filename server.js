b3691595007ad891f3a96aa33bd1f24ec2783774
merkaz-dev <merkaz.developer@gmail.com>
2020-12-29 23:23:19 +0300

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
 const express = require("express");
 const app = express();
+app.use(function (req, res, next) {
+  const allowedOrigins = [
+    "http://127.0.0.1:8020",
+    "http://localhost:4200",
+    "http://127.0.0.1:9000",
+    "http://localhost:5000",
+    "https://titan-survey-23b0a.web.app",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.get("/", (req, res) => {
   var ip =
